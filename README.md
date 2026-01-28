6f65183ac6552c07dda9fa6b66b6e1e6dd43689e
uoonsu1464 <uoonsu1464@gmail.com>
2026-01-28 01:38:56 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-A simple HTML/JS/CSS starter template
+# Lotto Number Generator
+
+A simple web application to generate 6 random lotto numbers between 1 and 45.
+
+## How to use
+
+1. Open `index.html` in your web browser.
+2. Click the "Generate Numbers" button.
+3. 6 unique random numbers will be displayed.
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// Add JS here
+const generateBtn = document.getElementById("generate");
+const numbersDiv = document.getElementById("numbers");
+
+generateBtn.addEventListener("click", () => {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+    numbersDiv.innerHTML = "";
+    for (const number of numbers) {
+        const span = document.createElement("span");
+        span.textContent = number;
+        numbersDiv.appendChild(span);
+    }
+});
